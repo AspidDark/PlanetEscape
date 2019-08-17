f7e01fe5eab8bb7001c4e9a594a79400438241a5
AspidDark <aupovTR@gmail.com>
2019-08-17 13:26:00 +0300

    Simple ZoomIn and ZoomOut script

---
+++ Assets/scripts/CameraWork/CameraMySettingsEngine.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraMySettingsEngine : MonoBehaviour {
+
+    public float closestZoomValue = 8;
+    public float farestZoomValue = 11;
+    public float zoomSpeed = 3;
+    public CinemachineVirtualCamera vcam;
+    public float speedZoomBorder = 15;
+
+    private float currentZoom=8;
+    private float zoom;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((currentZoom< farestZoomValue) && (AllObjectData.instance.speed > speedZoomBorder))
+        {
+            ZoomOut();
+        }
+        else if((currentZoom> closestZoomValue)&&(AllObjectData.instance.speed< speedZoomBorder))
+        {
+            ZoomIn();
+        }
+	}
+    //The same but we can variate zooum speed
+    private void ZoomOut()
+    {
+        currentZoom = vcam.m_Lens.OrthographicSize;
+        vcam.m_Lens.OrthographicSize+= zoomSpeed*MainCount.instance.fixedDeltaTime;
+
+    }
+    //The same but we can variate zooum speed
+    private void ZoomIn()
+    {
+        currentZoom = vcam.m_Lens.OrthographicSize;
+        vcam.m_Lens.OrthographicSize -= zoomSpeed * MainCount.instance.fixedDeltaTime;
+    }
+}
